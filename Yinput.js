e84448752ccff9bf92c69fbe8fcf3828a0979f98
TemaZaslavskii <tema.zaslavskii@gmail.com>
2024-11-27 04:16:38 +0700

    added ===

--- Yinput.js
+++ Yinput.js
@@ -1,7 +1,7 @@
 check_username = false
 
 function check() {
-    if (check_username == false) {
+    if (check_username === false) {
         var error_input_element = document.getElementById('error_input');
         error_input_element.style.display = 'block';
     }
