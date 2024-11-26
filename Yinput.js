20595628bffd86fe5ba6c9f6baf1274fe68a1601
TemaZaslavskii <tema.zaslavskii@gmail.com>
2024-11-27 04:11:28 +0700

    checking username at js

---
+++ Yinput.js
@@ -0,0 +1,11 @@
+check_username = false
+
+function check() {
+    if (check_username == false) {
+        var error_input_element = document.getElementById('error_input');
+        error_input_element.style.display = 'block';
+    }
+    else {
+
+    }
+}
