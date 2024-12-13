d6baec1a8515cafbe528c5811a1220184a8ac0e7
iqumi <iqumi3@yandex.ru>
2024-12-13 23:30:06 +0700

    feature/qumi/get_messages

--- server/main.py
+++ server/main.py
@@ -27,7 +27,31 @@ async def check_username(username: str):
 async def websocket_endpoint(websocket: WebSocket, user_id: str):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
+        data = await websocket.receive_json()
+        action = data["action"]
+
         chats = await ChatService.get_user_chats(user_id)
-        if data == "get_chats":
-            await websocket.send_text(chats)
+        if action == "get_chats":
+            await websocket.send_json(chats)
+        
+        elif action == "get_messages":
+            chat_id = data["chat_id"]
+            # список возвращаемых сообщений 
+            first_n_messages_to_return = []
+            # из генератора достаем сообщения
+            async for message in MessageService.get_chat_messages(data["chat_id"]):
+                # добавляем сообщение в список
+                first_n_messages_to_return.append(message)
+                # если в списке 10 сообщений
+                if len(first_n_messages_to_return) == 10:
+                    # отправляем клиенту
+                    await websocket.send_json(first_n_messages_to_return)
+                    # список очищаем
+                    first_n_messages_to_return = []
+                # после цикла если в списке еще остались сообщения
+            if len(first_n_messages_to_return) != 0:
+                # добейте выживших
+                await websocket.send_json(first_n_messages_to_return)
+                
+
+            
